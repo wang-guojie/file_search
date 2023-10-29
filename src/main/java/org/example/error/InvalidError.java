2af1f8dcb9cd22069139fc99630acc8e8fe6398f
王国杰 <2479744143@qq.com>
2023-10-29 15:15:56 +0800

    初始化文件搜索工具Java版本

--- src/main/java/org/example/error/InvalidError.java
+++ src/main/java/org/example/error/InvalidError.java
@@ -4,7 +4,7 @@ package org.example.error;
 /**
  * 这个是无效盘符异常
  */
-public class InvalidError extends CustomError{
+public class InvalidError extends CustomError {
 
 
     public InvalidError(String errorMessage) {
